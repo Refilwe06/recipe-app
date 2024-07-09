11eaa22433a3c9d06f91257a4bae9cd03fa32e11
refilwe06 <refilwemotseo2@gmail.com>
2024-07-09 10:50:08 +0200

    update

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ const App = () => {
 
         {
             title: 'Beef Curry',
-            shortDescription: 'Beef curry and serve with a hunk of naan bread to mop up the delicious juices.',
-            ingredients: ['2 tbsp oil', '500g diced braising steak', '1 tbsp butter', '1 large onion, chopped',' 2 garlic cloves, crushed', '1 thumb sized piece of ginger must be finely grated', '¼ tsp hot Chili Powder', '1 tsp turmeric', '2 tsp ground coriander', '3 cardamom pods, crushed', '400g can chopped tomatoes', '300ml beef stock', '1 tsp sugar', '2 tsp garam masala',],
-            preparationSteps: '1. Cook Beef. 2. Add spices. 3. Add tomatoes and cream. 4. Simmer.',
+            shortDescription: 'Beef curry and serve with rice to mop up the delicious juices.',
+            ingredients: ['2 tbsp oil', '500g diced braising steak', '1 tbsp butter', '1 large onion, chopped',' 2 garlic cloves, crushed', '1 thumb sized piece of ginger must be finely grated', '¼ tsp hot Chili Powder', '1 tsp turmeric', '2 tsp ground coriander', '3 cardamom pods, crushed', '300ml beef stock', '1 tsp sugar', '½ small bunch coriander, roughly chopped', 'naan bread or rice, to serve'],
+            preparationSteps: '1. Heat 2 tbsp oil in a pot. Add 500g diced braising steak and cook until browned.  2. Add 1 chopped large onion, 2 crushed garlic cloves, and 1 thumb-sized piece of finely grated ginger. 3.Cook until fragrant. Add ¼ tsp hot chili powder, 1 tsp turmeric, 2 tsp ground coriander, and 3 crushed cardamom pods. Stir well. 4. Add 300ml beef stock. Stir in 1 tsp sugar. 5. Bring to a boil, then reduce heat and simmer for 1.5 hours, or until beef is tender. 6. Serve hot with rice. ',
             picture: beefCurryImg,
         },
 
